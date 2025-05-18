0e62474e34b176cb6b5cb7c3fb27714cd7ac5bf6
Nguyenhoan03 <phephehoan@gmail.com>
2025-05-18 22:13:34 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code will go here
+    console.log('Website loaded successfully!');
+}); 
