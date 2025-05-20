0179e6f751600a114a9a005ef65afbf734330a5b
Nguyenhoan03 <phephehoan@gmail.com>
2025-05-20 16:52:08 +0700

    fasr

--- js/home/banner.js
+++ js/home/banner.js
@@ -1,12 +1,15 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+setInterval(function() {
+  plusSlides(1);
+}, 3000);
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -19,11 +22,15 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.opacity = 0;
+    slides[i].style.zIndex = 0;
+    slides[i].style.pointerEvents = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.opacity = 1;
+  slides[slideIndex-1].style.zIndex = 1;
+  slides[slideIndex-1].style.pointerEvents = "auto";
   if (dots[slideIndex-1]) dots[slideIndex-1].className += " active";
 }
